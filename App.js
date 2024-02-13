dd6cc2fef3775448930ad3dda8c1dc1bbc4f0153
kalyan <siripallivenkatakalyan@gmail.com>
2024-02-13 21:26:30 +0530

    npx create-react-app myapp

--- App.js
+++
@@ -1,6 +0,0 @@
-const root = ReactDOM.createRoot(document.getElementById("root"));
-const element = React.createElement("div", {}, [
-  React.createElement("div", {}, React.createElement("h1", {}, "heading-1")),
-  React.createElement("div", {}, React.createElement("h1", {}, "heading-2")),
-]);
-root.render(element);
